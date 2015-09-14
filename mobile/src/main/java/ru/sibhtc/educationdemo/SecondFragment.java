8f4805e0b85619351283b63ba0ce54a357bb5e67
FinkoAI <anton.finko@mail.ru>
2015-09-14 10:06:13 +0600

    Первичный коммит проекта

---
+++ mobile/src/main/java/ru/sibhtc/educationdemo/SecondFragment.java
@@ -0,0 +1,25 @@
+package ru.sibhtc.educationdemo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Антон on 13.09.2015.
+ */
+public class SecondFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+        return view;
+    }
+
+}
